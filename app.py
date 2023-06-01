ee96a8a913bb61c1b87f1ad33e47ff96f35f62c9
shravyareddy11 <shravyareddyg915@gmail.com.com>
2023-06-01 17:26:12 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+
+FAI=Flask(__name__)
+
+
+@FAI.route('/first')
+def first():
+    return render_template('first.html')
+
+
+@FAI.route('/data_render')
+def data_render():
+    return render_template('data_render.html',name='shravya',age=21)
+
+if __name__=='__main__':
+    FAI.run(debug=True)
+
